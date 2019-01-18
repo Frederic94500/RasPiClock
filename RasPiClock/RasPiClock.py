19bcc1e2557723485e8ba11d4e8d613772dfde3b
Frédéric TRAN <frederictran@hotmail.fr>
2019-01-18 23:50:51 +0100

    Stable Commit: ajout du Bitcoin

--- RasPiClock/RasPiClock.py
+++ RasPiClock/RasPiClock.py
@@ -2,20 +2,32 @@ import requests as rq
 import time
 import json
 
+from papirus import Papirus
+from papirus import PapirusComposite
+
 def Ordre(Order):
 	if Order == 0: Crypto()
 
 def Crypto():
 	RC = rq.get("https://chain.so/api/v2/get_info/ticker_BTC_USD")
-	text = json.loads(RC.text)
-	result = text["data"]
+	data = json.loads(RC.text)
+	result = data["data"]
 	price = list(str(result["price"]))
 	del price[-7:-1]
-	print("$" + "".join(price) + " | TX Unconfirmed: " + str(result["unconfirmed_txs"]))
+
+	TextEtImg.AddText("$ " + "".join(price), 64, 88, fontPath="Ubuntu.ttf")
+	TextEtImg.AddImg("BTC.bmp", 10, 66, (44,44))
+	TextEtImg.WriteAll()
+	Ecran.partial_update()
+
+	#print("$" + "".join(price) + " | TX Unconfirmed: " + str(result["unconfirmed_txs"]))
 	Order = 0
 	time.sleep(30)
 	Ordre(Order)
 
-prix()
+Ecran = Papirus()
+TextEtImg = PapirusComposite()
+
+Crypto()
 
 
