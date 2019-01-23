2fd1cba35075e58216183c228ac52d17d497cced
Frédéric TRAN <frederictran@hotmail.fr>
2019-01-23 16:27:27 +0100

    Décalage entre album et titre de musique quand le titre de la musique est trop grand + TEST météo sans true dans WriteAll

--- RasPiClock/RasPiClock.py
+++ RasPiClock/RasPiClock.py
@@ -75,7 +75,7 @@ def Meteo():
 	TextEtImg.AddText("Temp Min: " + str(DataMeteo["main"]["temp_min"]) + "°C" + " Temp Max: " + str(DataMeteo["main"]["temp_max"]) + "°C", 10, 65, size = 12, fontPath="Ubuntu.ttf")
 	TextEtImg.AddText("Temps: " + DataMeteo["weather"][0]["description"], 10, 85, size = 25, fontPath="Ubuntu.ttf") 
 
-	TextEtImg.WriteAll(True)
+	TextEtImg.WriteAll()
 
 	time.sleep(15)
 
@@ -91,16 +91,15 @@ def Musique():
 		if DataLast["recenttracks"]["track"][0]["@attr"]["nowplaying"] == "true":
 			TextEtImg.AddText("Actuellement:", 10, 40, size = 25, fontPath="Ubuntu.ttf")
 			TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
-			TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 80, size = 10, fontPath="Ubuntu.ttf")
+			TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
 
 			TextEtImg.AddText("Précédent:", 10, 115, size = 25, fontPath="Ubuntu.ttf")
 			TextEtImg.AddText(DataLast["recenttracks"]["track"][1]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][1]["name"], 10, 140, size = 15, fontPath="Ubuntu.ttf")
-			TextEtImg.AddText(DataLast["recenttracks"]["track"][1]["album"]["#text"], 10, 155, size = 10, fontPath="Ubuntu.ttf")
-	
+
 	except KeyError:
 		TextEtImg.AddText("Précédent:", 10, 40, size = 25, fontPath="Ubuntu.ttf")
 		TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
-		TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 80, size = 10, fontPath="Ubuntu.ttf")
+		TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
 
 	finally:
 
