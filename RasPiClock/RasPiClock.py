c31093e311261c365111412cf0a8a741ba62b0d5
Frédéric TRAN <frederictran@hotmail.fr>
2019-03-17 01:29:50 +0100

    Ajout de l'heure et apparition de la fonction APICheck

--- RasPiClock/RasPiClock.py
+++ RasPiClock/RasPiClock.py
@@ -20,6 +20,9 @@ def LibCheck():
 		print("ATTENTION, vous n'avez pas installé la biblothèque Papirus, veuillez l'installer") #Phrase temp
 		sys.exit()
 
+#def APICheck():
+
+
 def Main():
 	try:
 		while A == 0:
@@ -60,6 +63,8 @@ def Crypto():
 	TextEtImg.AddText("".join(PCTBTC) + "%", 64, 74, size = 15, fontPath="Ubuntu.ttf")
 	TextEtImg.AddText("".join(PCTETH) + "%", 64, 144, size = 15, fontPath="Ubuntu.ttf")
 
+	Time()
+
 	TextEtImg.WriteAll(True)
 	time.sleep(15)
 	TextEtImg.Clear()
@@ -72,6 +77,8 @@ def Meteo():
 	TextEtImg.AddText("Température: " + str(DataMeteo["main"]["temp"]) + "°C", 10, 40, size = 25, fontPath="Ubuntu.ttf")
 	TextEtImg.AddText("Temp. Min: " + str(DataMeteo["main"]["temp_min"]) + "°C" + " Temp. Max: " + str(DataMeteo["main"]["temp_max"]) + "°C", 10, 65, size = 12, fontPath="Ubuntu.ttf")
 	TextEtImg.AddText("Temps: " + DataMeteo["weather"][0]["description"], 10, 85, size = 25, fontPath="Ubuntu.ttf") 
+
+	Time()
 
 	TextEtImg.WriteAll(True)
 	time.sleep(15)
@@ -95,6 +102,7 @@ def Musique():
 		TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
 		TextEtImg.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
 	finally:
+		Time()
 		TextEtImg.WriteAll(True)
 		time.sleep(15)
 		TextEtImg.Clear()
@@ -123,13 +131,16 @@ def Social():
 	except IndexError:
 		TextEtImg.AddText("MisterMV: OFF", 10, 130, size = 20, fontPath="Ubuntu.ttf")
 	finally:
+		Time()
 		TextEtImg.WriteAll(True)
 		time.sleep(15)
 		TextEtImg.Clear()
+
+def Time():
+	Heure = time.strftime("%H:%M", time.localtime())
+	TextEtImg.AddText(Heure, 200, 10, size = 20, fontPath="Ubuntu.ttf")
 
 conf = configparser.ConfigParser()
 conf.read("config.cfg")
 
 Main()
-
-
