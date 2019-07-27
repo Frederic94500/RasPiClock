c85aa7e5e4a215b942685ca06716339adea74922
Frédéric TRAN <frederictran@hotmail.fr>
2019-07-28 00:58:50 +0200

    Correction de bug sur la liaison avec Services

--- RasPiClock/PrintScreen.py
+++ RasPiClock/PrintScreen.py
@@ -4,14 +4,14 @@ import Services as SV
 def Crypto(conf, TextPAPIRUS): #Fonction Crypto (CryproCompare)
 	SV.SVCrypto(conf)
 
-	PCTC1 = list(str(DataCrypto["RAW"][conf["CRYPTO"]["Coin1"]][conf["CRYPTO"]["Currency"]]["CHANGEPCT24HOUR"]))
+	PCTC1 = list(str(SV.DataCrypto["RAW"][conf["CRYPTO"]["Coin1"]][conf["CRYPTO"]["Currency"]]["CHANGEPCT24HOUR"]))
 	del PCTC1[-14:-1]
-	PCTC2 = list(str(DataCrypto["RAW"][conf["CRYPTO"]["Coin2"]][conf["CRYPTO"]["Currency"]]["CHANGEPCT24HOUR"]))
+	PCTC2 = list(str(SV.DataCrypto["RAW"][conf["CRYPTO"]["Coin2"]][conf["CRYPTO"]["Currency"]]["CHANGEPCT24HOUR"]))
 	del PCTC2[-14:-1]
 
 	TextPAPIRUS.AddText("Crypto:", 10, 10, size = 20, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText(conf["CRYPTO"]["Coin1"] + ": " + conf["CRYPTO"]["Currency"] + " " + str(DataCrypto["RAW"][conf["CRYPTO"]["Coin1"]][conf["CRYPTO"]["Currency"]]["PRICE"]), 10, 44, size = 25, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText(conf["CRYPTO"]["Coin2"] + ": " + conf["CRYPTO"]["Currency"] + " " + str(DataCrypto["RAW"][conf["CRYPTO"]["Coin2"]][conf["CRYPTO"]["Currency"]]["PRICE"]), 10, 114, size = 25, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText(conf["CRYPTO"]["Coin1"] + ": " + conf["CRYPTO"]["Currency"] + " " + str(SV.DataCrypto["RAW"][conf["CRYPTO"]["Coin1"]][conf["CRYPTO"]["Currency"]]["PRICE"]), 10, 44, size = 25, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText(conf["CRYPTO"]["Coin2"] + ": " + conf["CRYPTO"]["Currency"] + " " + str(SV.DataCrypto["RAW"][conf["CRYPTO"]["Coin2"]][conf["CRYPTO"]["Currency"]]["PRICE"]), 10, 114, size = 25, fontPath="Ubuntu.ttf")
 	TextPAPIRUS.AddText("".join(PCTC1) + "%", 10, 74, size = 15, fontPath="Ubuntu.ttf")
 	TextPAPIRUS.AddText("".join(PCTC2) + "%", 10, 144, size = 15, fontPath="Ubuntu.ttf")
 
@@ -25,9 +25,9 @@ def Meteo(conf, TextPAPIRUS, Units): #Fonction Météo (OpenWeatherMap)
 	SV.SVMeteo(conf)
 
 	TextPAPIRUS.AddText("Météo:", 10, 10, size = 20, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText("Température: " + str(DataMeteo["main"]["temp"]) + "°C", 10, 40, size = 25, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText("Temp. Min: " + str(DataMeteo["main"]["temp_min"]) + "°C" + " Temp. Max: " + str(DataMeteo["main"]["temp_max"]) + Units, 10, 65, size = 12, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText("Temps: " + DataMeteo["weather"][0]["description"].capitalize(), 10, 85, size = 25, fontPath="Ubuntu.ttf") 
+	TextPAPIRUS.AddText("Température: " + str(SV.DataMeteo["main"]["temp"]) + "°C", 10, 40, size = 25, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText("Temp. Min: " + str(SV.DataMeteo["main"]["temp_min"]) + "°C" + " Temp. Max: " + str(SV.DataMeteo["main"]["temp_max"]) + Units, 10, 65, size = 12, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText("Temps: " + SV.DataMeteo["weather"][0]["description"].capitalize(), 10, 85, size = 25, fontPath="Ubuntu.ttf") 
 
 	TextPAPIRUS.AddText(time.strftime("%H:%M", time.localtime()), 200, 10, size = 20, fontPath="Ubuntu.ttf")
 
@@ -41,17 +41,17 @@ def Musique(conf, TextPAPIRUS): #Fonction Musique (Last.fm)
 	TextPAPIRUS.AddText("Last.fm:", 10, 10, size = 20, fontPath="Ubuntu.ttf")
 
 	try:
-		if DataLast["recenttracks"]["track"][0]["@attr"]["nowplaying"] == "true":
+		if SV.DataLast["recenttracks"]["track"][0]["@attr"]["nowplaying"] == "true":
 			TextPAPIRUS.AddText("Actuellement:", 10, 40, size = 25, fontPath="Ubuntu.ttf")
-			TextPAPIRUS.AddText(DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
-			TextPAPIRUS.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
+			TextPAPIRUS.AddText(SV.DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + SV.DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
+			TextPAPIRUS.AddText(SV.DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
 
 			TextPAPIRUS.AddText("Précédent:", 10, 115, size = 25, fontPath="Ubuntu.ttf")
-			TextPAPIRUS.AddText(DataLast["recenttracks"]["track"][1]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][1]["name"], 10, 140, size = 15, fontPath="Ubuntu.ttf")
+			TextPAPIRUS.AddText(SV.DataLast["recenttracks"]["track"][1]["artist"]["#text"] + " - " + SV.DataLast["recenttracks"]["track"][1]["name"], 10, 140, size = 15, fontPath="Ubuntu.ttf")
 	except:
 		TextPAPIRUS.AddText("Précédent:", 10, 40, size = 25, fontPath="Ubuntu.ttf")
-		TextPAPIRUS.AddText(DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
-		TextPAPIRUS.AddText(DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
+		TextPAPIRUS.AddText(SV.DataLast["recenttracks"]["track"][0]["artist"]["#text"] + " - " + SV.DataLast["recenttracks"]["track"][0]["name"], 10, 65, size = 15, fontPath="Ubuntu.ttf")
+		TextPAPIRUS.AddText(SV.DataLast["recenttracks"]["track"][0]["album"]["#text"], 10, 95, size = 10, fontPath="Ubuntu.ttf")
 	finally:
 		TextPAPIRUS.AddText(time.strftime("%H:%M", time.localtime()), 200, 10, size = 20, fontPath="Ubuntu.ttf")
 		TextPAPIRUS.WriteAll(True)
@@ -64,22 +64,22 @@ def Twitch(conf, TextPAPIRUS): #Fonction Twitch
 	TextPAPIRUS.AddText("Twitch:", 10, 10, size = 20, fontPath="Ubuntu.ttf")
 
 	try:
-		if DataSt1["data"][0]["type"] == "live":
-			ReponseTwitchGameID = requests.get("https://api.twitch.tv/helix/games?id=" + DataSt1["data"][0]["game_id"], headers={"Client-ID": conf["TWITCH"]["TwitchAPI"]})
+		if SV.DataSt1["data"][0]["type"] == "live":
+			ReponseTwitchGameID = requests.get("https://api.twitch.tv/helix/games?id=" + SV.DataSt1["data"][0]["game_id"], headers={"Client-ID": conf["TWITCH"]["TwitchAPI"]})
 			GameID = json.loads(ReponseTwitchGameID.text)
 			TextPAPIRUS.AddText(conf["TWITCH"]["TwitchSt1"].capitalize() + ": ON", 10, 40, size = 20, fontPath="Ubuntu.ttf")
 			TextPAPIRUS.AddText("Jeu: " + GameID["data"][0]["name"], 10, 60, size = 15, fontPath="Ubuntu.ttf")
-			TextPAPIRUS.AddText("Titre: " + DataSt1["data"][0]["title"], 10, 75, size = 10, fontPath="Ubuntu.ttf")
+			TextPAPIRUS.AddText("Titre: " + SV.DataSt1["data"][0]["title"], 10, 75, size = 10, fontPath="Ubuntu.ttf")
 	except IndexError:
 		TextPAPIRUS.AddText(conf["TWITCH"]["TwitchSt1"].capitalize() + ": OFF", 10, 40, size = 20, fontPath="Ubuntu.ttf")
 		
 	try:
-		if DataSt2["data"][0]["type"] == "live":
-			ReponseTwitchGameID = requests.get("https://api.twitch.tv/helix/games?id=" + DataSt2["data"][0]["game_id"], headers={"Client-ID": conf["TWITCH"]["TwitchAPI"]})
+		if SV.DataSt2["data"][0]["type"] == "live":
+			ReponseTwitchGameID = requests.get("https://api.twitch.tv/helix/games?id=" + SV.DataSt2["data"][0]["game_id"], headers={"Client-ID": conf["TWITCH"]["TwitchAPI"]})
 			GameID = json.loads(ReponseTwitchGameID.text)
 			TextPAPIRUS.AddText(conf["TWITCH"]["TwitchSt2"].capitalize() + ": ON", 10, 95, size = 20, fontPath="Ubuntu.ttf")
 			TextPAPIRUS.AddText("Jeu: " + GameID["data"][0]["name"], 10, 115, size = 15, fontPath="Ubuntu.ttf")
-			TextPAPIRUS.AddText("Titre: " + DataSt2["data"][0]["title"], 10, 130, size = 10, fontPath="Ubuntu.ttf")
+			TextPAPIRUS.AddText("Titre: " + SV.DataSt2["data"][0]["title"], 10, 130, size = 10, fontPath="Ubuntu.ttf")
 	except IndexError:
 		TextPAPIRUS.AddText(conf["TWITCH"]["TwitchSt2"].capitalize() + ": OFF", 10, 132, size = 20, fontPath="Ubuntu.ttf")
 	finally:
@@ -93,11 +93,11 @@ def Twitter(conf, TextPAPIRUS, BearerAUTH): #Fonction Twitter
 
 	TextPAPIRUS.AddText("Twitter:", 10, 10, size = 20, fontPath="Ubuntu.ttf")
 
-	TextPAPIRUS.AddText("Compte: " + DataTwitter["name"], 10, 40, size = 20, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText(str(DataTwitter["followers_count"]) + " abonnés", 10, 65, size = 20, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText("Compte: " + SV.DataTwitter["name"], 10, 40, size = 20, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText(str(SV.DataTwitter["followers_count"]) + " abonnés", 10, 65, size = 20, fontPath="Ubuntu.ttf")
 
 	TextPAPIRUS.AddText("Dernier tweet:", 10, 85, size = 20, fontPath="Ubuntu.ttf")
-	TextPAPIRUS.AddText(DataTwitter["status"]["text"], 10, 105, size = 15, fontPath="Ubuntu.ttf")
+	TextPAPIRUS.AddText(SV.DataTwitter["status"]["text"], 10, 105, size = 15, fontPath="Ubuntu.ttf")
 
 	TextPAPIRUS.AddText(time.strftime("%H:%M", time.localtime()), 200, 10, size = 20, fontPath="Ubuntu.ttf")
 	TextPAPIRUS.WriteAll(True)
--- RasPiClock/RasPiClock.py
+++ RasPiClock/RasPiClock.py
@@ -95,9 +95,8 @@ if os.path.exists('/etc/default/epd-fuse'):
 		#Test des APIs
 		try:
 			if conf["WEATHER"]["MeteoAPI"] != "":
-				ReponseMeteo = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + conf["WEATHER"]["City"] + "&units=" + conf["WEATHER"]["Units"] + "&lang=" + conf["WEATHER"]["Lang"] + "&appid=" + conf["WEATHER"]["MeteoAPI"])
-				DataMeteo = json.loads(ReponseMeteo.text)
-				if 400 <= int(DataMeteo["cod"]) <= 599: 
+				SV.SVMeteo(conf)
+				if 400 <= int(SV.DataMeteo["cod"]) <= 599: 
 						ERROR = "Erreur dans la config Météo, veuillez vérifier votre saisie!"
 						ErrorConfig(ERROR)
 						return
@@ -107,10 +106,9 @@ if os.path.exists('/etc/default/epd-fuse'):
 				Check += 1
 
 			if conf["CRYPTO"]["CryptoAPI"] != "":
-				ReponseCrypto = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + conf["CRYPTO"]["Coin1"] + "," + conf["CRYPTO"]["Coin2"] + "&tsyms=" + conf["CRYPTO"]["Currency"] + "&api_key=" + conf["CRYPTO"]["CryptoAPI"])
-				DataCrypto = json.loads(ReponseCrypto.text)
+				SV.SVCrypto(conf)
 				try:
-					if DataCrypto["Response"] == "Error":
+					if SV.DataCrypto["Response"] == "Error":
 						ERROR = "Erreur dans la config Crypto, veuillez vérifier votre saisie!"
 						ErrorConfig(ERROR)
 						return
@@ -120,10 +118,9 @@ if os.path.exists('/etc/default/epd-fuse'):
 				Check += 1
 
 			if conf["LASTFM"]["LastFmAPI"] != "":
-				ReponseLastFM = requests.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + conf["LASTFM"]["UserFM"] + "&limit=1&format=json&api_key=" + conf["LASTFM"]["LastFmAPI"])
-				DataLast = json.loads(ReponseLastFM.text)
+				SV.SVMusique(conf)
 				try:
-					if 2 <= int(DataLast["error"]) <= 29:
+					if 2 <= int(SV.DataLast["error"]) <= 29:
 						ERROR = "Erreur dans la config LastFM, veuillez vérifier votre saisie!"
 						ErrorConfig(ERROR)
 						return
@@ -143,9 +140,8 @@ if os.path.exists('/etc/default/epd-fuse'):
 					return
 				finally:
 					try:
-						ReponseTwitter = requests.get("https://api.twitter.com/1.1/users/show.json?screen_name=" + conf["TWITTER"]["UserTW"], headers={'Authorization': "Bearer " + BearerAUTH})
-						DataTwitter = json.loads(ReponseTwitter.text)
-						if 49 <= int(DataTwitter["errors"][0]["code"]) <= 599:
+						SV.SVTwitter(conf, BearerAUTH)
+						if 49 <= int(SV.DataTwitter["errors"][0]["code"]) <= 599:
 							ERROR = "Erreur dans la config Twitter, veuillez vérifier votre saisie!"
 							ErrorConfig(ERROR)
 							return
