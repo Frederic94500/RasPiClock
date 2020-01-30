b406ad606ad8f27852de69da46b98387da920f32
Frédéric TRAN <frederictran@hotmail.fr>
2020-01-30 23:54:17 +0100

    Bash Only branch!

--- RasPiClock/RasPiClock.py
+++ RasPiClock/RasPiClock.py
@@ -1,11 +1,7 @@
 # -*- encoding: utf-8 -*-
 #RasPiClock - Frédéric94500
 
-import time, json, sys, os, requests, socket, configparser, hashlib, webbrowser, threading
-from tkinter import *
-from tkinter.messagebox import *
-from tkinter import ttk
-from PIL import Image, ImageTk
+import time, json, sys, os, requests, socket, configparser, hashlib, threading
 
 import PrintScreen as PS
 import Services as SV
@@ -33,12 +29,6 @@ if os.path.exists('/etc/default/epd-fuse'):
 			while True:
 				global STOP
 				#global SLEEP
-				#if SLEEP: #GUI ONLY
-					#je dors
-					#while SLEEP:
-						#if STOP:
-							#break;
-						#time.sleep(10)'''
 				"""if (time.strftime("%H", time.localtime()) >= conf["SLEEP"]["HStart"] or time.strftime("%H", time.localtime()) < conf["SLEEP"]["HEnd"]) and conf["SLEEP"]["HStart"] != "" and conf["SLEEP"]["HEnd"] != "":
 					i = 0
 					while time.strftime("%H", time.localtime()) >= conf["SLEEP"]["HStart"] or time.strftime("%H", time.localtime()) < conf["SLEEP"]["HEnd"]:
@@ -55,25 +45,18 @@ if os.path.exists('/etc/default/epd-fuse'):
 						i += 1
 					TextPAPIRUS.Clear()
 					break;"""
-				if STOP: #GUI ONLY
-					BoutonAfficher.configure(state=NORMAL)
-					BoutonArreter.configure(state=DISABLED)
-					Texte.set("Veuillez saisir vos informations")
-					os.system("papirus-clear")
-					break;
-				else:
-					if conf["CRYPTO"]["CryptoAPI"] != "":
-						PS.Crypto(conf, TextPAPIRUS)
-					if conf["WEATHER"]["MeteoAPI"] != "":
-						PS.Meteo(conf, TextPAPIRUS, Units)
-					if conf["LASTFM"]["LastFmAPI"] != "":
-						PS.Musique(conf, TextPAPIRUS)
-					if conf["TWITCH"]["TwitchAPI"] != "":
-						PS.Twitch(conf, TextPAPIRUS)
-					if conf["TWITTER"]["TwitterAPI"] != "" and conf["TWITTER"]["twitterapisecret"] != "":
-						PS.Twitter(conf, TextPAPIRUS, BearerAUTH)
-					if conf["RATP"]["typetransA"] != "" and conf["RATP"]["lineA"] != "" and conf["RATP"]["stationA"] != "":
-						PS.RATP(conf, TextPAPIRUS)
+				if conf["CRYPTO"]["CryptoAPI"] != "":
+					PS.Crypto(conf, TextPAPIRUS)
+				if conf["WEATHER"]["MeteoAPI"] != "":
+					PS.Meteo(conf, TextPAPIRUS, Units)
+				if conf["LASTFM"]["LastFmAPI"] != "":
+					PS.Musique(conf, TextPAPIRUS)
+				if conf["TWITCH"]["TwitchAPI"] != "":
+					PS.Twitch(conf, TextPAPIRUS)
+				if conf["TWITTER"]["TwitterAPI"] != "" and conf["TWITTER"]["twitterapisecret"] != "":
+					PS.Twitter(conf, TextPAPIRUS, BearerAUTH)
+				if conf["RATP"]["typetransA"] != "" and conf["RATP"]["lineA"] != "" and conf["RATP"]["stationA"] != "":
+					PS.RATP(conf, TextPAPIRUS)
 		except (ValueError, socket.error, socket.gaierror, socket.herror, socket.timeout): #Situation d'erreur de connexion
 			TextPAPIRUS.Clear()
 			TextPAPIRUS.AddText("ERREUR de connexion, nouvelle tentative de connexion dans: T", 10, 48, size = 20, fontPath="Ubuntu.ttf", Id="TimerErr")
@@ -83,7 +66,7 @@ if os.path.exists('/etc/default/epd-fuse'):
 				time.sleep(1)
 			TextPAPIRUS.Clear()
 			Main()
-		except KeyboardInterrupt: #BASH ONLY
+		except KeyboardInterrupt:
 			print("Vous avez arrêté le processus, nettoyage de l'écran")
 			os.system("papirus-clear")
 			sys.exit()
@@ -212,15 +195,8 @@ if os.path.exists('/etc/default/epd-fuse'):
 				HashSave()
 
 	def ErrorConfig(ERROR):
-		try:
-			if sys.argv[1] == "-bash" or sys.argv[1] == "-b": #BASH ONLY
-					print(ERROR)
-					sys.exit()
-		except IndexError: #GUI ONLY
-			WARN = showerror("Erreur!", ERROR)
-			Texte.set("Erreur dans l'éxécution!")
-			BoutonAfficher.configure(state=NORMAL)
-			BoutonArreter.configure(state=DISABLED)
+			print(ERROR)
+			sys.exit()
 
 	def Adaptation():
 		global Units
@@ -234,156 +210,8 @@ if os.path.exists('/etc/default/epd-fuse'):
 
 		Main()
 
-	try:
-		if sys.argv[1] == "-bash" or sys.argv[1] == "-b": #BASH ONLY
-			HashVerify()
-		else: #Erreur d'argument
-			print("Erreur, veuillez écrire -b ou -bash ou rien pour éxécuter le programme!")
-	except IndexError: #Par défaut
-		global ZoneTexte
-		TABList = ["tabCrypto", "tabMeteo", "tabMusic", "tabTwitch", "tabTwitter"]
-		TABText = ["Crypto", "Météo", "Musique", "Twitch", "Twitter"]
-		NBArg = [4, 4, 2, 3, 3]
-		TEXTEntry = [["Clé API", "Monnaie fiduciaire", "Cryptomonnaie 1", "Cryptomonnaie 2"],["Clé API", "Ville", "Unité", "Langue"],["Clé API", "Utilisateur"],["Clé API", "Streamer 1", "Streamer 2"],["Clé API", "Clé API Secret", "Utilisateur"]]
-		TEXTConfig = [["CryptoAPI", "Currency", "Coin1", "Coin2"],["MeteoAPI", "City", "Units", "Lang"],["LastFmAPI", "UserFM"],["TwitchAPI", "TwitchSt1", "TwitchSt2"],["TwitterAPI", "TwitterAPISecret", "UserTW"]]
-		CONFCat = ["CRYPTO", "WEATHER", "LASTFM", "TWITCH", "TWITTER"]
-		ZoneTexte = ["CryptoAPI", "Currency", "Coin1", "Coin2", "MeteoAPI", "City", "Units", "Lang", "LastFmAPI", "UserFM", "TwitchAPI", "TwitchSt1", "TwitchSt2", "TwitterAPI", "TwitterAPISecret", "UserTW"]
-		I2 = 0
-		def Save(): #Fonction d'enregistrement du fichier de conf
-			I2 = 0
-
-			for I0 in range(5):
-				for I1 in range(NBArg[I0]):
-					conf[CONFCat[I0]][TEXTConfig[I0][I1]] = ZoneTexte[I2].get()
-					I2 += 1
-
-			with open('config.cfg', 'w') as CFGF:
-				conf.write(CFGF)
-
-			HashVerify()
-
-		def Afficher(): #Fonction de démarrage
-			BoutonAfficher.configure(state=DISABLED)
-			BoutonArreter.configure(state=NORMAL)
-			Texte.set("En cours d'éxécution")
-
-			global STOP
-			STOP = False
-			threadRas = threading.Thread(target=Save)
-			threadRas.start()
-		
-		def Arret(): #Fonction d'arrêt
-			BoutonArreter.configure(state=DISABLED)
-			Texte.set("En cours d'arrêt")
-
-			global STOP
-			STOP = True
 	
-		#Fonctions Sites web
-		def WebProj():
-			webbrowser.open_new_tab('https://github.com/Frederic94500/Resistance-ISN')
-
-		def WebAuteurF(event):
-			webbrowser.open_new_tab('https://twitter.com/Frederic94500')
-		def WebAuteurE(event):
-			webbrowser.open_new_tab('https://github.com/EliottCheypeplus')
-		def WebAuteurP(event):
-			webbrowser.open_new_tab('https://github.com/ParsaEtz')
-
-		def WebLicence(event):
-			webbrowser.open_new_tab('https://github.com/Frederic94500/RasPiClock-ISN/blob/master/LICENSE')
-		
-		def APropos(): #Fonction à propos (créateur + licence)(ouvre une nouvelle fenêtre)
-			About = Toplevel()
-			About.title("A propos et licence")
-
-			AbText = [0, 1, 2, 3, 4, 5]
-
-			AbText[0] = Label(About, text = "Ce programme a été fait par")
-			AbText[1] = Label(About, text = "Frédéric94500,", fg = "blue", cursor = "hand2")
-			AbText[2] = Label(About, text = "Eliott,", fg = "blue", cursor = "hand2")
-			AbText[3] = Label(About, text = "Parsa", fg = "blue", cursor = "hand2")
-			AbText[4] = Label(About, text = "sous la licence")
-			AbText[5] = Label(About, text = "GPL-3.0", fg = "blue", cursor = "hand2")
-
-			AbText[1].bind("<Button-1>", WebAuteurF)
-			AbText[2].bind("<Button-1>", WebAuteurE)
-			AbText[3].bind("<Button-1>", WebAuteurP)
-			AbText[5].bind("<Button-1>", WebLicence)
-
-			[AbText[I].pack(side = "left", pady = 10) for I in range(6)]
-
-			photo = ImageTk.PhotoImage(Image.open("gpl.png"))
-			img = Label(About, image=photo)
-			img.image = photo
-			img.pack()
-
-			ico = ImageTk.PhotoImage(file='icon.ico')
-			Fenetre.tk.call('wm', 'iconphoto', About._w, ico)
-
-		#Création Fenètre
-		Fenetre = Tk()
-		Fenetre.title('RasPiClock GUI')
-
-		ico = ImageTk.PhotoImage(file='icon.ico')
-		Fenetre.tk.call('wm', 'iconphoto', Fenetre._w, ico)
-
-		#Création barre de menu
-		menubar = Menu(Fenetre)
-
-		filemenu = Menu(menubar, tearoff=0)
-		menubar.add_cascade(label = "Fichier", menu = filemenu)
-		filemenu.add_command(label = "Quitter", command = Fenetre.destroy)
-
-		helpmenu = Menu(menubar, tearoff=0)
-		menubar.add_cascade(label = "Aide", menu = helpmenu)
-		helpmenu.add_command(label = "Vistez le GitHub", command = WebProj)
-		helpmenu.add_separator()
-		helpmenu.add_command(label = "A propos et licence", command = APropos)
-
-		Fenetre.config(menu=menubar)
-
-		#Création onglets
-		TAB = ttk.Notebook(Fenetre)
-
-		for I in range(5):
-			TABList[I] = ttk.Frame(TAB)
-			TAB.add(TABList[I], text=TABText[I])
-
-		TAB.pack(expand=1, fill='both')
-
-		#Création Grid
-		for I0 in range(5): #Nombre de tab
-			for I1 in range(NBArg[I0]): #Nombre d'argument
-				Label(TABList[I0], text=TEXTEntry[I0][I1]).grid(row=I1, padx=5, pady=5)
-
-				ARGUMENT = StringVar()
-				ARGUMENT.set(conf[CONFCat[I0]][TEXTConfig[I0][I1]])
-				ZoneTexte[I2] = Entry(TABList[I0], textvariable=ARGUMENT)
-				ZoneTexte[I2].grid(row=I1, column=1, padx=5, pady=5, sticky=E)
-				I2 += 1
-
-		#Création bouton fenetre
-		BoutonAfficher = Button(Fenetre, text='Afficher', command = Afficher, state=NORMAL)
-		BoutonAfficher.pack(side = RIGHT, padx=5, pady=5)
-
-		BoutonArreter = Button(Fenetre, text='Arrêter', command = Arret, state=DISABLED)
-		BoutonArreter.pack(side = RIGHT, padx=5, pady=5)
-
-		#Création Texte Annonce
-		Texte = StringVar()
-		TextAnnonce = Label(Fenetre, textvariable = Texte).pack(side = LEFT)
-		Texte.set("Veuillez saisir vos informations")
-
-		#Initialisation du GUI
-		Fenetre.mainloop()
+	HashVerify()
 
 else: #Si papirus n'est pas installé
-	try:
-		if sys.argv[1] == "-bash" or sys.argv[1] == "-b": #BASH ONLY
-			print("Attention!, vous n'avez pas installé la biblothèque Papirus, veuillez l'installer via https://github.com/PiSupply/PaPiRus")
-			sys.exit()
-		else: #Erreur d'argument
-			print("Erreur, veuillez écrire -b ou -bash ou rien et installer Papirus (https://github.com/PiSupply/PaPiRus) pour éxécuter le programme!")
-	except IndexError: #GUI ONLY
-		WARN = showerror("Attention!", "Vous n'avez pas installé la biblothèque Papirus, veuillez l'installer via https://github.com/PiSupply/PaPiRus.")
+	print("Erreur, veuillez écrire -b ou -bash ou rien et installer Papirus (https://github.com/PiSupply/PaPiRus) pour éxécuter le programme!")
